fd00be1df17b9fd5999cf76b45cc0588a8d64eab
nov1 <ffirex@gmail.com>
2017-02-04 18:02:25 +0200

    First commit - basic mvc architicture

---
+++ public/index.php
@@ -0,0 +1,43 @@
+<?
+
+/**
+ * Front contoller
+ */
+
+
+// echo "Requested URL = ". $_SERVER['QUERY_STRING'];
+
+
+/**
+ * Routing
+ */
+
+require '../Core/Router.php';
+
+$router = new Router();
+
+// echo get_class($router);
+
+//add the routes
+$router->add('', ['contoller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['contoller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['contoller' => 'Posts', 'action' => 'new']);
+
+//Display the routing table
+// echo '<pre>';
+// var_dump($router->getRoutes());
+// echo '</pre>';
+
+
+
+//Match the requested route
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+	echo '<pre>';
+	var_dump($router->getParams());
+	echo '</pre>';
+}else{
+	echo "No route found for URL '$url'";
+}
+?>
