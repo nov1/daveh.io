9cbef507ab04da72f33afcfabfe62a124e05b361
nov1 <ffirex@yandex.ru>
2017-02-06 01:16:08 +0200

    3rd folder of mvc lessons finished

--- Core/Router.php
+++ Core/Router.php
@@ -12,8 +12,16 @@ protected $params = [];
 
 
 
-public function add($route, $params)
+public function add($route, $params = [])
 {
+	// Convert the route to a regular expression: escape forward slashes
+	$route = preg_replace('/\//', '\\/', $route);
+	// Convert variables e.g. {controller}
+	$route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+	// Convert variables with custom regular expressions e.g. {id:\d+}
+	$route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+	// Add start and end delimiters, and case insensitive flag
+	$route = '/^' . $route . '$/i';
 	$this->routes[$route] = $params;
 }
 
@@ -23,19 +31,22 @@ public function getRoutes()
 	return $this->routes;
 }
 
-
 public function match($url)
 {
-	foreach ($this->routes as $route => $params) {
-		if ($url == $route) {
-			$this->params = $params;
-			return true;
-		}
-	}
-	return false;
+    foreach ($this->routes as $route => $params) {
+        if (preg_match($route, $url, $matches)) {
+            // Get named capture group values
+            foreach ($matches as $key => $match) {
+                if (is_string($key)) {
+                    $params[$key] = $match;
+                }
+            }
+            $this->params = $params;
+            return true;
+        }
+    }
+    return false;
 }
-
-
 
 public function getParams()
 {
--- public/index.php
+++ public/index.php
@@ -19,9 +19,11 @@ $router = new Router();
 // echo get_class($router);
 
 //add the routes
-$router->add('', ['contoller' => 'Home', 'action' => 'index']);
-$router->add('posts', ['contoller' => 'Posts', 'action' => 'index']);
-$router->add('posts/new', ['contoller' => 'Posts', 'action' => 'new']);
+// $router->add('', ['contoller' => 'Home', 'action' => 'index']);
+// $router->add('posts', ['contoller' => 'Posts', 'action' => 'index']);
+// $router->add('posts/new', ['contoller' => 'Posts', 'action' => 'new']);
+$router->add('{contoller}/{action}');
+$router->add('{contoller}/{id:\d+}/{action}');
 
 //Display the routing table
 // echo '<pre>';
@@ -32,10 +34,10 @@ $router->add('posts/new', ['contoller' => 'Posts', 'action' => 'new']);
 
 //Match the requested route
 $url = $_SERVER['QUERY_STRING'];
-
 if ($router->match($url)) {
 	echo '<pre>';
 	var_dump($router->getParams());
+	var_dump($router->getRoutes());
 	echo '</pre>';
 }else{
 	echo "No route found for URL '$url'";
